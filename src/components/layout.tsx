86e2a26a71a6f953e14c9a127e11eee86398ad1f
Ploy <117616170+yaminP@users.noreply.github.com>
2024-02-21 18:03:43 +0700

    loginform

---
+++ src/components/layout.tsx
@@ -0,0 +1,20 @@
+import { Sriracha } from 'next/font/google'
+
+export const sriracha = Sriracha({
+  weight:'400',
+  subsets: ['latin'],
+})
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <section className={`${sriracha.className}`}>
+      <main className={`min-h-screen flex justify-center md:items-center px-5 py-14 md:p-24`}>
+        {children}
+      </main>
+    </section>
+  )
+}
---
+++ src/pages/loginForm.tsx
@@ -0,0 +1,26 @@
+import Layout from "@/components/layout";
+import Link from "next/link";
+
+export default function LoginForm() {
+    return (
+        <Layout>
+            <div className="conrainer ">
+                {/* <h2 className="text-center font-black text-2xl">ระบบตรวจสอบการโอนเงิน</h2> */}
+                <div className="bg-white p-7 max-w-md mx-auto rounded-md relative mt-20  shadow-md " >
+                    <div className="bg-black rounded-full overflow-hidden w-20 h-20 absolute -top-10 shadow-md ">
+                        <img className="w-full h-full absolute" src="https://i.pinimg.com/564x/b1/7e/c8/b17ec8045c7cf08566c50d6e0e506158.jpg" alt="" />
+                    </div>
+                    <div className="mt-10">
+                        <p>Username:</p>
+                        <input type="text" className="text-base p-1.5 w-full focus:ring-0 border-b" />
+                        <p className="mt-5">Password:</p>
+                        <input type="text" className="text-base p-1.5 w-full focus:ring-0 border-b" />
+                        <button type="submit" className="bg-purple-400 hover:bg-purple-500 w-full rounded-2xl mt-9 p-1 drop-shadow-lg">
+                            <Link href='/' className="font-bold text-purple-100">Login</Link>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
