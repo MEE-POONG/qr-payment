45c47137c84d6aa7ffa18ce88b98f45ab9c9175e
Ploy <117616170+yaminP@users.noreply.github.com>
2024-03-02 16:04:47 +0700

    imgslider

---
+++ src/components/datatest.ts
@@ -0,0 +1,33 @@
+export const imgslide =[
+    {
+        id: 1,
+        src: "/images/chinjang.jpg",
+        // title: "ชินจัง",
+        // desc:"A Is Charactor 1",
+        name:"ชินจัง",
+        phone:"000 999 8875",
+        ig:"chin_jang",
+        fb:"Chin Jang",
+
+    },
+    {
+        id: 2,
+        src: "/images/qrcode.png",
+        // title: "Image B",
+        // desc:"B Is Charactor 2",
+        name:"ชินจัง",
+        phone:"000 999 8875",
+        ig:"chin_jang",
+        fb:"Chin Jang",
+    },
+    {
+        id: 3,
+        src: "/images/qrcode.png",
+        // title: "Image C",
+        // desc:"C Is Charactor 3",
+        name:"ชินจัง",
+        phone:"000 999 8875",
+        ig:"chin_jang",
+        fb:"Chin Jang",
+    },
+]
