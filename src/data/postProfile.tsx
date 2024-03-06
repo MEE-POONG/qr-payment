3199bd3399bc08bed237bce0ffc627e8d14b11a1
devilzeros <devilzeros@outlook.co.th>
2024-03-06 14:24:11 +0700

    est

--- src/data/postProfile.tsx
+++ src/data/postProfile.tsx
@@ -3,16 +3,16 @@ export const GalleryData = [
     { id: "2", postProfile: "2" },
 ];
 export const postProfile = [
-    { id: "1", name: "หมวย แซบปาก", caption: "จะกินข้าวหรือกินเรา ?", facebook: "muay", instagram: "w.a.x1" },
-    { id: "2", name: "สปอย แซบปาก", caption: "จะกินข้าวหรือกินเรา ?", facebook: "spoy", instagram: "w.a.x1" },
+    { id: "1", name: "หมวย แซบปาก", caption: "จะกินข้าวหรือกินเรา ?", facebook: "muay", instagram: "w.a.x1", template: "1" },
+    { id: "2", name: "สปอย แซบปาก", caption: "จะกินข้าวหรือกินเรา ?", facebook: "spoy", instagram: "w.a.x1", template: "2" },
 ];
 export const ImageData = [
-    { id: "1", GalleryID: "1",src:"https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp", },
-    { id: "1", GalleryID: "1",src:"https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp", },
-    { id: "1", GalleryID: "1",src:"https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp", },
-    { id: "1", GalleryID: "1",src:"https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(76).webp", },
-    { id: "1", GalleryID: "1",src:"https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp", },
-    { id: "1", GalleryID: "1",src:"https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(78).webp", },
+    { id: "1", GalleryID: "1", src: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp", },
+    { id: "1", GalleryID: "1", src: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp", },
+    { id: "1", GalleryID: "1", src: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp", },
+    { id: "1", GalleryID: "1", src: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(76).webp", },
+    { id: "1", GalleryID: "1", src: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp", },
+    { id: "1", GalleryID: "1", src: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(78).webp", },
 ];
 
 
