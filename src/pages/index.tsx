43682a1cab18a00cb0f4ecf7b8226a8e58b0ea6d
devilzeros <devilzeros@outlook.co.th>
2024-03-12 15:42:22 +0700

    est

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,12 @@
 import DarkImageBackground from "@/components/BG";
-import SliderIndex from "@/components/Slider";
-import Slide from "@/components/Slider";
 import SliderClothSquare from "@/components/Slider/ClothSquare";
 import Image from "next/image";
-import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
-// import Slide from '../components/Slide';
 
 const Home: React.FC = () => {
 
   return (
     <div className="relative w-screen h-screen ">
       <DarkImageBackground />
-      {/* GalleryTemplate 1 */}
-      {/* <SliderIndex /> */}
-      {/* GalleryTemplate 2 */}
       <SliderClothSquare />
       <div className="absolute bg-white p-2 bottom-0 m-4 text-center">
         <p className="text-2xl font-bold text-pink-600 drop-shadow-lg">Scan Me</p>
