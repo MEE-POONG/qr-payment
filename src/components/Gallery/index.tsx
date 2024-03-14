3f1e4ecd9f50a8d9815f1233fa6f122d1a9543e2
devilzeros <devilzeros@outlook.co.th>
2024-03-14 10:25:41 +0700

    est

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LayoutTem from './layoutTem';
+import LayoutTem from './LayoutTem';
 
 const GalleryIndex: React.FC = () => {
     return (
@@ -29,11 +29,11 @@ const GalleryIndex: React.FC = () => {
                             className="block w-full h-full rounded-lg object-cover object-center border-2 border-indigo-500 hover:border-yellow-700"
                             src={"https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/f701ce08-7ebe-4af2-c4ec-2b3967392900/350"} />
                     </div>
-                    <div className="w-ful h-[60%] p-1 md:p-2 relative">
+                    <div className="w-full h-[60%] p-1 md:p-2 border-1 border relative">
                         <span className='absolute top-2/4 left-2/4 translate-y-[-50%] translate-x-[-50%] font-bold'>เลือกรูป</span>
                         <img
                             alt="gallery"
-                            className="block h-full w-full rounded-lg object-cover object-center border-2 border-indigo-500 hover:border-yellow-700"
+                            className="block w-full h-full rounded-lg object-cover object-center border-2 border-indigo-500 hover:border-yellow-700"
                             src={"https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/f701ce08-7ebe-4af2-c4ec-2b3967392900/350"} />
                     </div>
                 </div>
