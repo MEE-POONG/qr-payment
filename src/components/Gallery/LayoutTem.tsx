a54c87908ade76682337f22cc7c797a1c2780145
devilzeros <devilzeros@outlook.co.th>
2024-03-20 01:00:54 +0700

    est

--- src/components/Gallery/LayoutTem.tsx
+++ src/components/Gallery/LayoutTem.tsx
@@ -32,19 +32,8 @@ const GallerySelect: React.FC = () => {
         <>
             <DarkImageBackground />
             <div className='container m-auto flex h-full flex-wrap'>
-                <div className="h-auto flex flex-col w-full z-50 lg:w-[40%] py-2 px-1 rounded-lg p-4 ">
-                    <div className="w-full flex-grow bg-white rounded-lg p-4 h-max ">
-                        {Object.keys(userInfo).map((key) => {
-                            const safeKey = key as keyof typeof userInfo; // Correctly type 'key' as a key of 'userInfo'
-                            return (
-                                <div className='mb-2' key={key}>
-                                    <span className="block text-lg font-medium text-slate-700">{key}</span>
-                                    <input type="text" id={key} value={userInfo[safeKey]} onChange={handleInputChange}
-                                        className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
-                                        placeholder={`Enter value ${key}`} required />
-                                </div>
-                            );
-                        })}
+                <div className="flex flex-col w-full z-50 lg:w-[40%] py-2 px-1 rounded-lg p-4 ">
+                    <div className="w-full flex-grow bg-white rounded-lg p-4">
                         <div className='mb-2'>
                             <span className="block text-lg font-medium text-slate-700 ">เลือกเทมเพลต</span>
                             <div id="select_gallery" className='flex w-full h-max justify-center text-3xl'>
@@ -58,12 +47,40 @@ const GallerySelect: React.FC = () => {
                                     <FaChevronRight />
                                 </button>
                             </div>
+                            <div className='hidden md:block '>
+                                {Object.keys(userInfo).map((key) => {
+                                    const safeKey = key as keyof typeof userInfo; // Correctly type 'key' as a key of 'userInfo'
+                                    return (
+                                        <div className='mb-2' key={key}>
+                                            <span className="block text-lg font-medium text-slate-700">{key}</span>
+                                            <input type="text" id={key} value={userInfo[safeKey]} onChange={handleInputChange}
+                                                className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
+                                                placeholder={`Enter value ${key}`} required />
+                                        </div>
+                                    );
+                                })}
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="h-full flex flex-col w-full lg:w-[60%] py-2 px-1">
+                <div className="h-[60%] flex flex-col w-full md:h-[100%] lg:w-[60%] py-2 px-1">
                     <GalleryIndex mode={'edit'} selectTem={currentTemIndex} />
                     <BoxText data={userInfo} />
+                </div>
+                <div className="h-auto flex flex-col w-full z-50 lg:w-[40%] py-2 px-1 rounded-lg p-4 md:hidden">
+                    <div className="w-full flex-grow bg-white rounded-lg p-4 h-max ">
+                        {Object.keys(userInfo).map((key) => {
+                            const safeKey = key as keyof typeof userInfo; // Correctly type 'key' as a key of 'userInfo'
+                            return (
+                                <div className='mb-2' key={key}>
+                                    <span className="block text-lg font-medium text-slate-700">{key}</span>
+                                    <input type="text" id={key} value={userInfo[safeKey]} onChange={handleInputChange}
+                                        className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
+                                        placeholder={`Enter value ${key}`} required />
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         </>
