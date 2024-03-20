c38374ac930d168f4b43367012d371288aa71180
devilzeros <devilzeros@outlook.co.th>
2024-03-20 18:10:38 +0700

    est

--- src/pages/api/profiledata/poststory.ts
+++ src/pages/api/profiledata/poststory.ts
@@ -5,14 +5,34 @@ const prisma = new PrismaClient();
 
 // Define an interface for the image object structure
 interface Image {
-  number: number;
-  src: string;
+    number: number;
+    src: string;
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { method } = req;
 
     switch (method) {
+        case 'GET':
+            try {
+                // Fetch all profiles including their related ImageData and Payment records
+                const profiles = await prisma.profileData.findMany({
+                    include: {
+                        ImageData: true, // Include all related ImageData records
+                        Payment: true, // Include all related Payment records
+                    },
+                });
+
+                if (!profiles) {
+                    return res.status(404).json({ error: "Profiles not found" });
+                }
+
+                res.status(200).json(profiles);
+            } catch (error) {
+                console.error("Failed to retrieve profiles:", error);
+                res.status(500).json({ error: "An error occurred while fetching the profiles" });
+            }
+            break;
         case 'POST':
             try {
                 const { name, caption, facebook, instagram, line, galleryTemplate, selectedImages } = req.body;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,93 @@
 import DarkImageBackground from "@/components/BG";
+import GalleryIndex from "@/components/Gallery";
+import BoxText from "@/components/Gallery/BoxText";
+import Layout from "@/components/layout";
 import SliderClothSquare from "@/components/Slider/ClothSquare";
+import { GalleryTemData } from "@/data/gallery";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
+interface ImageData {
+  id: string;
+  number: string;
+  src: string;
+  createdAt: string;
+  updatedAt: string;
+  postProfileID: string;
+}
+
+interface Payment {
+  id: string;
+  amount: number;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  postProfileID: string;
+}
+
+interface Profile {
+  id: string;
+  name: string;
+  caption: string;
+  facebook: string;
+  instagram: string;
+  line: string;
+  galleryTemplate: number;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: null | string;
+  deletedBy: null | string;
+  ImageData: ImageData[];
+  Payment: Payment[];
+}
 
 const Home: React.FC = () => {
+  const [galleryTemplate, setGalleryTemplate] = useState(0);
+  const [selectedImages, setSelectedImages] = useState<string[]>(Array(GalleryTemData[0].imglist.length).fill(""));
+  const [userInfo, setUserInfo] = useState({
+    caption: '',
+    name: '',
+    facebook: '',
+    instagram: '',
+    line: '',
+  });
+  const [profiles, setProfiles] = useState<Profile[]>([]);
+
+  useEffect(() => {
+    const fetchProfiles = async () => {
+      const response = await fetch('/api/profiledata/poststory');
+      const data = await response.json();
+      setProfiles(data);
+    };
+
+    fetchProfiles();
+  }, []);
+  useEffect(() => {
+    console.log(profiles);
+  }, [profiles]);
+
 
   return (
-    <div className="relative w-screen h-screen ">
-      <DarkImageBackground />
+    <Layout>
       <SliderClothSquare />
+      {/* <div className='container m-auto flex h-full flex-wrap justify-center'>
+        {profiles.map((profile) => (
+          <div key={profile.id} className="h-[60%] flex flex-col w-full md:h-[100%] lg:w-[60%] py-2 px-1">
+            <GalleryIndex
+              mode={'view'}
+              selectTem={profile.galleryTemplate}
+              selectedImages={profile.ImageData.map(image => image.src)}
+              updateSelectedImages={setSelectedImages}
+            />
+            <BoxText data={profile} />
+          </div>
+        ))}
+      </div> */}
       <div className="absolute bg-white p-2 bottom-0 m-4 text-center">
         <p className="text-2xl font-bold text-pink-600 drop-shadow-lg">Scan Me</p>
         <Image src='/images/qrcode.png' width={100} height={100} alt="" className="mx-auto w-44" />
       </div>
-    </div>
+    </Layout>
   );
 }
 export default Home;
