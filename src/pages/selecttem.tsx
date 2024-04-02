e43506372a069724f1f89d7b0a9f2676bb0c1bd1
devilzeros <devilzeros@outlook.co.th>
2024-04-02 17:01:01 +0700

    est

--- src/pages/selecttem.tsx
+++ src/pages/selecttem.tsx
@@ -171,46 +171,7 @@ const SelectTem: React.FC = () => {
             </div>
           </div>
         </div>
-        {/* <div className='w-full bg-white'>
-          <UserInfoForm userInfo={userInfo} handleInputChange={handleInputChange} handleSubmit={handleSubmit} />
-        </div> */}
       </div>
-      {/* <div className='container m-auto flex h-full flex-wrap'>
-        <div className="flex flex-col w-full md:w-[20rem] z-50 py-2 px-1 rounded-lg p-4 ">
-          <div className="w-full flex-grow bg-white rounded-lg p-4">
-            <span className="block text-lg font-medium text-slate-700 ">เลือกเทมเพลต</span>
-            <div id="select_gallery" className='flex w-full h-max justify-center text-3xl'>
-              <button onClick={() => navigateTemplate('prev')} className="py-2">
-                <FaChevronLeft />
-              </button>
-              <div className="text-center py-2">
-                Tem {galleryTemplate + 1}
-              </div>
-              <button onClick={() => navigateTemplate('next')} className="py-2">
-                <FaChevronRight />
-              </button>
-            </div>
-            <div className='hidden md:block '>
-              <UserInfoForm userInfo={userInfo} handleInputChange={handleInputChange} handleSubmit={handleSubmit} />
-            </div>
-          </div>
-        </div>
-        <div className={`${isUserInfoPopulated() ? `h-[50%]` : `h-[45%]`} flex flex-col w-full md:h-[100%] lg:w-[60%] py-2 px-1`}>
-          <GalleryIndex
-            mode={'edit'}
-            selectTem={galleryTemplate}
-            selectedImages={selectedImages}
-            updateSelectedImages={setSelectedImages}
-            updateImageCount={updateImageCount}
-          />
-          <BoxText data={userInfo} />
-        </div>
-        <div className="h-auto flex flex-col w-full z-50 lg:w-[40%] py-2 px-1 rounded-lg p-4 md:hidden">
-          <div className="w-full flex-grow bg-white rounded-lg p-4 h-max ">
-            <UserInfoForm userInfo={userInfo} handleInputChange={handleInputChange} handleSubmit={handleSubmit} />
-          </div>
-        </div>
-      </div> */}
     </Layout >
   );
 }
